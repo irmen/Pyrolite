c4927d92ec5cb539d18770d5b4caaf7e19b30b93
irmen <irmen@08cfa0ad-1c00-4aa0-9110-f4b10de7e716>
2013-06-22 12:27:49 +0000

    java: SimpleExample now talks to python echo server

--- examples/SimpleExample.java
+++ examples/SimpleExample.java
@@ -1,16 +1,38 @@
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
 import net.razorvine.pyro.*;
 
 public class SimpleExample {
 
     public static void main(String[] args) throws IOException
     {
+    	// First, Make sure you have the builtin echoserver running, with name server enabled:
+    	// $ python -m Pyro4.test.echoserver -Nv
+    	// Then run this example client.
+    	
         NameServerProxy ns = NameServerProxy.locateNS(null);
-        PyroProxy remoteobject = new PyroProxy(ns.lookup("Your.Pyro.Object"));
+        PyroProxy remoteobject = new PyroProxy(ns.lookup("test.echoserver"));
 
-        Object result = remoteobject.call("pythonmethod", 42, "hello", new int[]{1,2,3});
-        String message = (String)result;   // cast to the type that 'pythonmethod' returns
-        System.out.println("result message="+message);
+        
+		// simple remote echo call
+        String echomessage = (String) remoteobject.call("echo", "hello there");
+        System.out.println("echo response: "+echomessage);
+        
+        // more complex call, pass a dict as argument
+        Map<String, Object> argument = new HashMap<String, Object>();
+        argument.put("value", 42);
+        argument.put("message", "hello");
+        argument.put("timestamp", new java.util.Date());
+        Object obj = remoteobject.call("echo", argument);
+        
+        System.out.println("complex echo response: "+obj);
+        Map<String, Object> result = (Map<String, Object>)obj;
+        System.out.println("value="+result.get("value"));
+        System.out.println("message="+result.get("messge"));
+        System.out.println("timestamp=" + ((Calendar)result.get("timestamp")).getTime());
 
         remoteobject.close();
         ns.close();
