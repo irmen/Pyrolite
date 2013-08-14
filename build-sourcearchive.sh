c2215e003fc0cb742167f1d6905039e31915a08a
Irmen de Jong <irmen@razorvine.net>
2013-08-14 16:20:12 +0200

    source can be directly downloaded from github

--- build-sourcearchive.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if [ -d build/pyrolite-export ]; then
-	rm -r build/pyrolite-export
-fi
-svn export . build/pyrolite-export
-tar czf build/pyrolite-src.tar.gz -C build pyrolite-export
-rm -r build/pyrolite-export
-
