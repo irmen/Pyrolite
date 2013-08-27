ed7f00430d132cd01b7829457497583cceb9c011
Irmen de Jong <irmen@razorvine.net>
2013-08-27 04:46:21 +0200

    indentation fix

--- dotnet/Pyrolite.Tests/Pyro/MessageTests.cs
+++ dotnet/Pyrolite.Tests/Pyro/MessageTests.cs
@@ -55,7 +55,7 @@ public class MessageTestsHmac {
 	public void setUp() {
 		Config.HMAC_KEY = Encoding.ASCII.GetBytes("testsuite");
 		this.ser = new PickleSerializer();
-        // this.ser = Pyro4.util.get_serializer(Pyro4.config.SERIALIZER)  @TODO
+		// this.ser = Pyro4.util.get_serializer(Pyro4.config.SERIALIZER)  @TODO
 	}
 
 	[TestFixtureTearDown]
@@ -86,47 +86,47 @@ public class MessageTestsHmac {
 		new Message(99, new byte[0], this.ser.serializer_id, 0, 0, null);  // doesn't check msg type here
 		Assert.Throws(typeof(PyroException), ()=>Message.from_header(Encoding.ASCII.GetBytes("FOOBAR")));
 		var msg = new Message(Message.MSG_CONNECT, Encoding.ASCII.GetBytes("hello"), this.ser.serializer_id, 0, 0, null);
-        Assert.AreEqual(Message.MSG_CONNECT, msg.type);
-        Assert.AreEqual(5, msg.data_size);
-        Assert.AreEqual(new byte[]{(byte)'h',(byte)'e',(byte)'l',(byte)'l',(byte)'o'}, msg.data);
-        Assert.AreEqual(4+2+20, msg.annotations_size);
-        byte[] mac = pyrohmac(Encoding.ASCII.GetBytes("hello"), msg.annotations);
-        var expected = new Dictionary<string, byte[]>();
-        expected["HMAC"] = mac;
-        CollectionAssert.AreEqual(expected, msg.annotations);
+		Assert.AreEqual(Message.MSG_CONNECT, msg.type);
+		Assert.AreEqual(5, msg.data_size);
+		Assert.AreEqual(new byte[]{(byte)'h',(byte)'e',(byte)'l',(byte)'l',(byte)'o'}, msg.data);
+		Assert.AreEqual(4+2+20, msg.annotations_size);
+		byte[] mac = pyrohmac(Encoding.ASCII.GetBytes("hello"), msg.annotations);
+		var expected = new Dictionary<string, byte[]>();
+		expected["HMAC"] = mac;
+		CollectionAssert.AreEqual(expected, msg.annotations);
 
-        byte[] hdr = msg.to_bytes().Take(Message.HEADER_SIZE).ToArray();
-        msg = Message.from_header(hdr);
-        Assert.AreEqual(Message.MSG_CONNECT, msg.type);
-        Assert.AreEqual(4+2+20, msg.annotations_size);
-        Assert.AreEqual(5, msg.data_size);
+		byte[] hdr = msg.to_bytes().Take(Message.HEADER_SIZE).ToArray();
+		msg = Message.from_header(hdr);
+		Assert.AreEqual(Message.MSG_CONNECT, msg.type);
+		Assert.AreEqual(4+2+20, msg.annotations_size);
+		Assert.AreEqual(5, msg.data_size);
 
-        hdr = new Message(Message.MSG_RESULT, new byte[0], this.ser.serializer_id, 0, 0, null).to_bytes().Take(Message.HEADER_SIZE).ToArray();
-        msg = Message.from_header(hdr);
-        Assert.AreEqual(Message.MSG_RESULT, msg.type);
-        Assert.AreEqual(4+2+20, msg.annotations_size);
-        Assert.AreEqual(0, msg.data_size);
+		hdr = new Message(Message.MSG_RESULT, new byte[0], this.ser.serializer_id, 0, 0, null).to_bytes().Take(Message.HEADER_SIZE).ToArray();
+		msg = Message.from_header(hdr);
+		Assert.AreEqual(Message.MSG_RESULT, msg.type);
+		Assert.AreEqual(4+2+20, msg.annotations_size);
+		Assert.AreEqual(0, msg.data_size);
 
-        hdr = new Message(Message.MSG_RESULT, Encoding.ASCII.GetBytes("hello"), 12345, 60006, 30003, null).to_bytes().Take(Message.HEADER_SIZE).ToArray();
-        msg = Message.from_header(hdr);
-        Assert.AreEqual(Message.MSG_RESULT, msg.type);
-        Assert.AreEqual(60006, msg.flags);
-        Assert.AreEqual(5, msg.data_size);
-        Assert.AreEqual(12345, msg.serializer_id);
-        Assert.AreEqual(30003, msg.seq);
+		hdr = new Message(Message.MSG_RESULT, Encoding.ASCII.GetBytes("hello"), 12345, 60006, 30003, null).to_bytes().Take(Message.HEADER_SIZE).ToArray();
+		msg = Message.from_header(hdr);
+		Assert.AreEqual(Message.MSG_RESULT, msg.type);
+		Assert.AreEqual(60006, msg.flags);
+		Assert.AreEqual(5, msg.data_size);
+		Assert.AreEqual(12345, msg.serializer_id);
+		Assert.AreEqual(30003, msg.seq);
 
-        byte[] data = new Message(255, new byte[0], this.ser.serializer_id, 0, 255, null).to_bytes();
-        Assert.AreEqual(50, data.Length);
-        data = new Message(1, new byte[0], this.ser.serializer_id, 0, 255, null).to_bytes();
-        Assert.AreEqual(50, data.Length);
-        data = new Message(1, new byte[0], this.ser.serializer_id, 253, 254, null).to_bytes();
-        Assert.AreEqual(50, data.Length);
+		byte[] data = new Message(255, new byte[0], this.ser.serializer_id, 0, 255, null).to_bytes();
+		Assert.AreEqual(50, data.Length);
+		data = new Message(1, new byte[0], this.ser.serializer_id, 0, 255, null).to_bytes();
+		Assert.AreEqual(50, data.Length);
+		data = new Message(1, new byte[0], this.ser.serializer_id, 253, 254, null).to_bytes();
+		Assert.AreEqual(50, data.Length);
 
-        // compression is a job of the code supplying the data, so the messagefactory should leave it untouched
-        data = new byte[1000];
-        byte[] msg_bytes1 = new Message(Message.MSG_INVOKE, data, this.ser.serializer_id, 0, 0, null).to_bytes();
-        byte[] msg_bytes2 = new Message(Message.MSG_INVOKE, data, this.ser.serializer_id, Message.FLAGS_COMPRESSED, 0, null).to_bytes();
-        Assert.AreEqual(msg_bytes1.Length, msg_bytes2.Length);
+		// compression is a job of the code supplying the data, so the messagefactory should leave it untouched
+		data = new byte[1000];
+		byte[] msg_bytes1 = new Message(Message.MSG_INVOKE, data, this.ser.serializer_id, 0, 0, null).to_bytes();
+		byte[] msg_bytes2 = new Message(Message.MSG_INVOKE, data, this.ser.serializer_id, Message.FLAGS_COMPRESSED, 0, null).to_bytes();
+		Assert.AreEqual(msg_bytes1.Length, msg_bytes2.Length);
 	}
 	
 	[Test]
@@ -137,13 +137,13 @@ public class MessageTestsHmac {
 		
 		var msg = new Message(Message.MSG_CONNECT, new byte[]{1,2,3,4,5}, this.ser.serializer_id, 0, 0, annotations);
 		byte[] data = msg.to_bytes();
-        int annotations_size = 4+2+20 + 4+2+5;
-        Assert.AreEqual(Message.HEADER_SIZE + 5 + annotations_size, data.Length);
-        Assert.AreEqual(annotations_size, msg.annotations_size);
-        Assert.AreEqual(2, msg.annotations.Count);
-        Assert.AreEqual(new byte[]{10,20,30,40,50}, msg.annotations["TEST"]);
-        byte[] mac = pyrohmac(new byte[]{1,2,3,4,5}, annotations);
-        Assert.AreEqual(mac, msg.annotations["HMAC"]);
+		int annotations_size = 4+2+20 + 4+2+5;
+		Assert.AreEqual(Message.HEADER_SIZE + 5 + annotations_size, data.Length);
+		Assert.AreEqual(annotations_size, msg.annotations_size);
+		Assert.AreEqual(2, msg.annotations.Count);
+		Assert.AreEqual(new byte[]{10,20,30,40,50}, msg.annotations["TEST"]);
+		byte[] mac = pyrohmac(new byte[]{1,2,3,4,5}, annotations);
+		Assert.AreEqual(mac, msg.annotations["HMAC"]);
 	}
 	
 	[Test]
@@ -153,17 +153,17 @@ public class MessageTestsHmac {
 			var anno = new Dictionary<string, byte[]>();
 			anno["TOOLONG"] = new Byte[]{10,20,30};
 			var msg = new Message(Message.MSG_CONNECT, new byte[]{1,2,3,4,5}, this.ser.serializer_id, 0, 0, anno);
-            byte[]data = msg.to_bytes();
-            Assert.Fail("should fail, too long");
+			byte[]data = msg.to_bytes();
+			Assert.Fail("should fail, too long");
 		} catch(ArgumentException) {
 			//ok
 		}
 		try {
 			var anno = new Dictionary<string, byte[]>();
 			anno["QQ"] = new Byte[]{10,20,30};
-            var msg = new Message(Message.MSG_CONNECT, new byte[]{1,2,3,4,5}, this.ser.serializer_id, 0, 0, anno);
-            byte[] data = msg.to_bytes();
-            Assert.Fail("should fail, too short");
+			var msg = new Message(Message.MSG_CONNECT, new byte[]{1,2,3,4,5}, this.ser.serializer_id, 0, 0, anno);
+			byte[] data = msg.to_bytes();
+			Assert.Fail("should fail, too short");
 		} catch (ArgumentException) {
 			//ok
 		}
@@ -175,23 +175,23 @@ public class MessageTestsHmac {
 		var annotations = new Dictionary<string, byte[]>();
 		annotations["TEST"] = new Byte[]{10, 20,30,40,50};
 		var msg = new Message(Message.MSG_CONNECT, new byte[]{1,2,3,4,5}, this.ser.serializer_id, 0, 0, annotations);
-        var c = new ConnectionMock();
-        c.send(msg.to_bytes());
-        msg = Message.recv(c, null);
-        Assert.AreEqual(0, c.RemainingLength);
-        Assert.AreEqual(5, msg.data_size);
-        Assert.AreEqual(new byte[]{1,2,3,4,5}, msg.data);
-        Assert.AreEqual(new byte[]{10,20,30,40,50}, msg.annotations["TEST"]);
-        Assert.IsTrue(msg.annotations.ContainsKey("HMAC"));
+		var c = new ConnectionMock();
+		c.send(msg.to_bytes());
+		msg = Message.recv(c, null);
+		Assert.AreEqual(0, c.RemainingLength);
+		Assert.AreEqual(5, msg.data_size);
+		Assert.AreEqual(new byte[]{1,2,3,4,5}, msg.data);
+		Assert.AreEqual(new byte[]{10,20,30,40,50}, msg.annotations["TEST"]);
+		Assert.IsTrue(msg.annotations.ContainsKey("HMAC"));
 	}
 	
 	[Test]
 	[ExpectedException(typeof(PyroException), ExpectedMessage="invalid protocol version: 25390")]
 	public void testProtocolVersion()
 	{
-        byte[] msg = new Message(Message.MSG_RESULT, new byte[0], this.ser.serializer_id, 0, 1, null).to_bytes().Take(Message.HEADER_SIZE).ToArray();
-        msg[4] = 99; // screw up protocol version in message header
-        Message.from_header(msg);
+		byte[] msg = new Message(Message.MSG_RESULT, new byte[0], this.ser.serializer_id, 0, 1, null).to_bytes().Take(Message.HEADER_SIZE).ToArray();
+		msg[4] = 99; // screw up protocol version in message header
+		Message.from_header(msg);
 	}
 	
 	[Test]
@@ -202,65 +202,65 @@ public class MessageTestsHmac {
 		byte[] data;
 		
 		try {
-            Config.HMAC_KEY = Encoding.ASCII.GetBytes("test key");
-            data = new Message(Message.MSG_RESULT, new byte[]{1,2,3,4,5}, 42, 0, 1, null).to_bytes();
-            c = new ConnectionMock(data);
+			Config.HMAC_KEY = Encoding.ASCII.GetBytes("test key");
+			data = new Message(Message.MSG_RESULT, new byte[]{1,2,3,4,5}, 42, 0, 1, null).to_bytes();
+			c = new ConnectionMock(data);
 		}
 		finally {
-            Config.HMAC_KEY = hk;
+			Config.HMAC_KEY = hk;
 		}
-        // test checking of different hmacs
-        try {
-            Message.recv(c, null);
-            Assert.Fail("crash expected");
-        }
-        catch(PyroException x) {
-        	Assert.IsTrue(x.Message.Contains("hmac"));
-        }
-        c = new ConnectionMock(data);
-        // test that it works again when resetting the key
-        try {
-            hk = Config.HMAC_KEY;
-            Config.HMAC_KEY = Encoding.ASCII.GetBytes("test key");
-            Message.recv(c, null);
-        }
-        finally {
-            Config.HMAC_KEY = hk;
-        }
-        c = new ConnectionMock(data);
-        // test that it doesn't work when no key is set
-        try {
-            hk = Config.HMAC_KEY;
-            Config.HMAC_KEY = null;
-            Message.recv(c, null);
-            Assert.Fail("crash expected");
-        }
-        catch(PyroException x) {
-        	Assert.IsTrue(x.Message.Contains("hmac key config"));
-        }
-        finally {
-            Config.HMAC_KEY = hk;
-        }
+		// test checking of different hmacs
+		try {
+			Message.recv(c, null);
+			Assert.Fail("crash expected");
+		}
+		catch(PyroException x) {
+			Assert.IsTrue(x.Message.Contains("hmac"));
+		}
+		c = new ConnectionMock(data);
+		// test that it works again when resetting the key
+		try {
+			hk = Config.HMAC_KEY;
+			Config.HMAC_KEY = Encoding.ASCII.GetBytes("test key");
+			Message.recv(c, null);
+		}
+		finally {
+			Config.HMAC_KEY = hk;
+		}
+		c = new ConnectionMock(data);
+		// test that it doesn't work when no key is set
+		try {
+			hk = Config.HMAC_KEY;
+			Config.HMAC_KEY = null;
+			Message.recv(c, null);
+			Assert.Fail("crash expected");
+		}
+		catch(PyroException x) {
+			Assert.IsTrue(x.Message.Contains("hmac key config"));
+		}
+		finally {
+			Config.HMAC_KEY = hk;
+		}
 	}
 	
 	[Test]
 	public void testChecksum()
 	{
 		var msg = new Message(Message.MSG_RESULT, new byte[]{1,2,3,4}, 42, 0, 1, null);
-        var c = new ConnectionMock();
-        c.send(msg.to_bytes());
-        // corrupt the checksum bytes
-        byte[] data = c.ReceivedData;
-        data[Message.HEADER_SIZE-2] = 0;
-        data[Message.HEADER_SIZE-1] = 0;
-        c = new ConnectionMock(data);
-        try {
-            Message.recv(c, null);
-            Assert.Fail("crash expected");
-        }
-        catch(PyroException x) {
-        	Assert.IsTrue(x.Message.Contains("checksum"));
-        }
+		var c = new ConnectionMock();
+		c.send(msg.to_bytes());
+		// corrupt the checksum bytes
+		byte[] data = c.ReceivedData;
+		data[Message.HEADER_SIZE-2] = 0;
+		data[Message.HEADER_SIZE-1] = 0;
+		c = new ConnectionMock(data);
+		try {
+			Message.recv(c, null);
+			Assert.Fail("crash expected");
+		}
+		catch(PyroException x) {
+			Assert.IsTrue(x.Message.Contains("checksum"));
+		}
 	}
 }
 
@@ -272,14 +272,14 @@ public class MessageTestsNoHmac {
 	public void testRecvNoAnnotations()
 	{
 		var msg = new Message(Message.MSG_CONNECT, new byte[]{1,2,3,4,5}, 42, 0, 0, null);
-        var c = new ConnectionMock();
-        c.send(msg.to_bytes());
-        msg = Message.recv(c, null);
-        Assert.AreEqual(0, c.RemainingLength);
-        Assert.AreEqual(5, msg.data_size);
-        Assert.AreEqual(new byte[]{1,2,3,4,5}, msg.data);
-        Assert.AreEqual(0, msg.annotations_size);
-        Assert.AreEqual(0, msg.annotations.Count);
+		var c = new ConnectionMock();
+		c.send(msg.to_bytes());
+		msg = Message.recv(c, null);
+		Assert.AreEqual(0, c.RemainingLength);
+		Assert.AreEqual(5, msg.data_size);
+		Assert.AreEqual(new byte[]{1,2,3,4,5}, msg.data);
+		Assert.AreEqual(0, msg.annotations_size);
+		Assert.AreEqual(0, msg.annotations.Count);
 	}
 }
 
--- dotnet/Pyrolite/Pyro/Message.cs
+++ dotnet/Pyrolite/Pyro/Message.cs
@@ -15,7 +15,7 @@ namespace Razorvine.Pyro
 /// </summary>
 public class Message
 {
-    private const int CHECKSUM_MAGIC = 0x34E9;
+	private const int CHECKSUM_MAGIC = 0x34E9;
 	public const int HEADER_SIZE = 24;
 
 	public const ushort MSG_CONNECT = 1;
@@ -32,8 +32,8 @@ public class Message
 	public const ushort SERIALIZER_JSON = 2;
 	public const ushort SERIALIZER_MARSHAL = 3;
 	public const ushort SERIALIZER_PICKLE = 4;
-		
-    public ushort type;
+	
+	public ushort type;
 	public ushort flags;
 	public byte[] data;
 	public int data_size;
@@ -43,14 +43,14 @@ public class Message
 	public IDictionary<string, byte[]> annotations;
 	
 	/// <summary>
-	/// construct a header-type mssage, without data and annotations payload.
+	/// construct a header-type message, without data and annotations payload.
 	/// </summary>
 	public Message(ushort msgType, ushort serializer_id, ushort flags, ushort seq)
 	{
-        this.type = msgType;
-        this.flags = flags;
-        this.seq = seq;
-        this.serializer_id = serializer_id;
+		this.type = msgType;
+		this.flags = flags;
+		this.seq = seq;
+		this.serializer_id = serializer_id;
 	}
 
 	/// <summary>
@@ -59,16 +59,16 @@ public class Message
 	public Message(ushort msgType, byte[] databytes, ushort serializer_id, ushort flags, ushort seq, IDictionary<string, byte[]> annotations)
 		:this(msgType, serializer_id, flags, seq)
 	{
-        this.data = databytes;
-        this.data_size = databytes.Length;
-        this.annotations = annotations;
-        if(null==annotations)
-        	this.annotations = new Dictionary<string, byte[]>(0);
-        
-        if(Config.HMAC_KEY!=null)
-        	this.annotations["HMAC"] = this.hmac();
-        
-        this.annotations_size = (ushort) this.annotations.Sum(a=>a.Value.Length+6);
+		this.data = databytes;
+		this.data_size = databytes.Length;
+		this.annotations = annotations;
+		if(null==annotations)
+			this.annotations = new Dictionary<string, byte[]>(0);
+		
+		if(Config.HMAC_KEY!=null)
+			this.annotations["HMAC"] = this.hmac();
+		
+		this.annotations_size = (ushort) this.annotations.Sum(a=>a.Value.Length+6);
 	}
 	
 	/// <summary>
@@ -105,9 +105,9 @@ public class Message
 	public byte[] get_header_bytes()
 	{
 		int checksum = (type+Config.PROTOCOL_VERSION+data.Length+annotations_size+serializer_id+flags+seq+CHECKSUM_MAGIC)&0xffff;
-        byte[] header = new byte[HEADER_SIZE];
-        /*
-         header format: '!4sHHHHiHHHH' (24 bytes)
+		byte[] header = new byte[HEADER_SIZE];
+		/*
+		 header format: '!4sHHHHiHHHH' (24 bytes)
 		   4   id ('PYRO')
 		   2   protocol version
 		   2   message type
@@ -121,41 +121,41 @@ public class Message
 		   followed by annotations: 4 bytes type, annotations bytes.
 		*/
 
-    	header[0]=(byte)'P';
-    	header[1]=(byte)'Y';
-    	header[2]=(byte)'R';
-    	header[3]=(byte)'O';
+		header[0]=(byte)'P';
+		header[1]=(byte)'Y';
+		header[2]=(byte)'R';
+		header[3]=(byte)'O';
 
-    	header[4]=(byte) (Config.PROTOCOL_VERSION>>8);
-    	header[5]=(byte) (Config.PROTOCOL_VERSION&0xff);
+		header[4]=(byte) (Config.PROTOCOL_VERSION>>8);
+		header[5]=(byte) (Config.PROTOCOL_VERSION&0xff);
 
-    	header[6]=(byte) (type>>8);
-    	header[7]=(byte) (type&0xff);
+		header[6]=(byte) (type>>8);
+		header[7]=(byte) (type&0xff);
 
-    	header[8]=(byte) (flags>>8);
-    	header[9]=(byte) (flags&0xff);
+		header[8]=(byte) (flags>>8);
+		header[9]=(byte) (flags&0xff);
 
-    	header[10]=(byte)(seq>>8);
-    	header[11]=(byte)(seq&0xff);
+		header[10]=(byte)(seq>>8);
+		header[11]=(byte)(seq&0xff);
 
-    	header[12]=(byte)((data.Length>>24)&0xff);
-    	header[13]=(byte)((data.Length>>16)&0xff);
-    	header[14]=(byte)((data.Length>>8)&0xff);
-    	header[15]=(byte)(data.Length&0xff);
+		header[12]=(byte)((data.Length>>24)&0xff);
+		header[13]=(byte)((data.Length>>16)&0xff);
+		header[14]=(byte)((data.Length>>8)&0xff);
+		header[15]=(byte)(data.Length&0xff);
 
-    	header[16]=(byte)(serializer_id>>8);
-    	header[17]=(byte)(serializer_id&0xff);
+		header[16]=(byte)(serializer_id>>8);
+		header[17]=(byte)(serializer_id&0xff);
 
-    	header[18]=(byte)((annotations_size>>8)&0xff);
-    	header[19]=(byte)(annotations_size&0xff);
-    	
-    	header[20]=0; // reserved
-    	header[21]=0; // reserved
-    	
-    	header[22]=(byte)((checksum>>8)&0xff);
-    	header[23]=(byte)(checksum&0xff);
+		header[18]=(byte)((annotations_size>>8)&0xff);
+		header[19]=(byte)(annotations_size&0xff);
+		
+		header[20]=0; // reserved
+		header[21]=0; // reserved
+		
+		header[22]=(byte)((checksum>>8)&0xff);
+		header[23]=(byte)(checksum&0xff);
 
-    	return header;
+		return header;
 	}
 	
 	public byte[] get_annotations_bytes()
@@ -182,24 +182,24 @@ public class Message
 		if(header==null || header.Length!=HEADER_SIZE)
 			throw new PyroException("header data size mismatch");
 		
-    	if(header[0]!='P'||header[1]!='Y'||header[2]!='R'||header[3]!='O') 
-    		throw new PyroException("invalid message");    		
+		if(header[0]!='P'||header[1]!='Y'||header[2]!='R'||header[3]!='O') 
+			throw new PyroException("invalid message");    		
 
-    	int version = (header[4] << 8)|header[5];
-    	if(version!=Config.PROTOCOL_VERSION)
-    		throw new PyroException("invalid protocol version: "+version);
-    	
-    	int msg_type = (header[6] << 8)|header[7];
-    	int flags = (header[8] << 8)|header[9];
-    	int seq = (header[10] << 8)|header[11];
-    	int data_size=(((((header[12] <<8) | header[13]) <<8) | header[14]) <<8) | header[15];
-    	int serializer_id = (header[16] << 8)|header[17];
+		int version = (header[4] << 8)|header[5];
+		if(version!=Config.PROTOCOL_VERSION)
+			throw new PyroException("invalid protocol version: "+version);
+		
+		int msg_type = (header[6] << 8)|header[7];
+		int flags = (header[8] << 8)|header[9];
+		int seq = (header[10] << 8)|header[11];
+		int data_size=(((((header[12] <<8) | header[13]) <<8) | header[14]) <<8) | header[15];
+		int serializer_id = (header[16] << 8)|header[17];
 		int annotations_size = (header[18]<<8)|header[19];
 		// byte 20 and 21 are reserved.
 		int checksum = (header[22]<<8)|header[23];
 		if(checksum!=((msg_type+version+data_size+annotations_size+flags+serializer_id+seq+CHECKSUM_MAGIC)&0xffff))
-            throw new PyroException("header checksum mismatch");
-    	
+			throw new PyroException("header checksum mismatch");
+		
 		var msg = new Message((ushort)msg_type, (ushort)serializer_id, (ushort)flags, (ushort)seq);
 		msg.data_size = data_size;
 		msg.annotations_size = (ushort)annotations_size;
@@ -208,9 +208,9 @@ public class Message
 
 	
 	// Note: this 'chunked' way of sending is not used because it triggers Nagle's algorithm
-    // on some systems (linux). This causes massive delays, unless you change the socket option
-    // TCP_NODELAY to disable the algorithm. What also works, is sending all the message bytes
-    // in one go: connection.send(message.to_bytes())
+	// on some systems (linux). This causes massive delays, unless you change the socket option
+	// TCP_NODELAY to disable the algorithm. What also works, is sending all the message bytes
+	// in one go: connection.send(message.to_bytes())
 //    public void send(Stream connection)
 //    {
 //    	// send the message as bytes over the connection
@@ -219,13 +219,13 @@ public class Message
 //    		IOUtil.send(connection, get_annotations_bytes());
 //    	IOUtil.send(connection, data);
 //	}
-    
-    
+	
+	
 	/// <summary>
 	/// Receives a pyro message from a given connection.
-    /// Accepts the given message types (None=any, or pass a sequence).
-    /// Also reads annotation chunks and the actual payload data.
-    /// Validates a HMAC chunk if present.
+	/// Accepts the given message types (None=any, or pass a sequence).
+	/// Also reads annotation chunks and the actual payload data.
+	/// Validates a HMAC chunk if present.
 	/// </summary>
 	public static Message recv(Stream connection, ushort[] requiredMsgTypes)
 	{
@@ -235,41 +235,41 @@ public class Message
 			throw new PyroException(string.Format("invalid msg type {0} received", msg.type));
 
 		byte[] annotations_data = null;
-	    msg.annotations = new Dictionary<string, byte[]>();
+		msg.annotations = new Dictionary<string, byte[]>();
 		if(msg.annotations_size>0)
 		{
-		    // read annotation chunks
-		    annotations_data = IOUtil.recv(connection, msg.annotations_size);
-		    int i=0;
-		    while(i<msg.annotations_size)
-		    {
-		    	string anno = Encoding.ASCII.GetString(annotations_data, i, 4);
-		    	int length = (annotations_data[i+4]<<8) | annotations_data[i+5];
-		    	byte[] annotations_bytes = new byte[length];
-		    	Array.Copy(annotations_data, i+6, annotations_bytes, 0, length);
-		    	msg.annotations[anno] = annotations_bytes;
-                i += 6+length;
-		    }
+			// read annotation chunks
+			annotations_data = IOUtil.recv(connection, msg.annotations_size);
+			int i=0;
+			while(i<msg.annotations_size)
+			{
+				string anno = Encoding.ASCII.GetString(annotations_data, i, 4);
+				int length = (annotations_data[i+4]<<8) | annotations_data[i+5];
+				byte[] annotations_bytes = new byte[length];
+				Array.Copy(annotations_data, i+6, annotations_bytes, 0, length);
+				msg.annotations[anno] = annotations_bytes;
+				i += 6+length;
+			}
 		}
 		
 		// read data
-        msg.data = IOUtil.recv(connection, msg.data_size);
-            	
-        if(Config.MSG_TRACE_DIR!=null) {
+		msg.data = IOUtil.recv(connection, msg.data_size);
+				
+		if(Config.MSG_TRACE_DIR!=null) {
 			TraceMessageRecv(msg.seq, header_data, annotations_data, msg.data);
 		}
-        
-        if(msg.annotations.ContainsKey("HMAC") && Config.HMAC_KEY!=null)
-        {
-        	if(!msg.annotations["HMAC"].SequenceEqual<byte>(msg.hmac()))
-                throw new PyroException("message hmac mismatch");
-        }
-        else if (msg.annotations.ContainsKey("HMAC") != (Config.HMAC_KEY!=null))
-        {
-            // Message contains hmac and local HMAC_KEY not set, or vice versa. This is not allowed.
-            throw new PyroException("hmac key config not symmetric");
-        }
-        return msg;
+		
+		if(msg.annotations.ContainsKey("HMAC") && Config.HMAC_KEY!=null)
+		{
+			if(!msg.annotations["HMAC"].SequenceEqual<byte>(msg.hmac()))
+				throw new PyroException("message hmac mismatch");
+		}
+		else if (msg.annotations.ContainsKey("HMAC") != (Config.HMAC_KEY!=null))
+		{
+			// Message contains hmac and local HMAC_KEY not set, or vice versa. This is not allowed.
+			throw new PyroException("hmac key config not symmetric");
+		}
+		return msg;
 	}
 
 	public static void TraceMessageRecv(int sequenceNr, byte[] headerdata, byte[] annotations, byte[] data) {
