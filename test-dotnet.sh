98c6d600f2d1a44448abc9b741f150af2cbf719f
Irmen de Jong <irmen@razorvine.net>
2013-09-02 22:42:29 +0200

    run nunit with framework 4.0

--- test-dotnet.sh
+++ test-dotnet.sh
@@ -7,4 +7,4 @@ echo "Running tests"
 # note: nunit-2.6.2 crashes on Mono. Stick with 2.6.1 for the time being.
 NUNIT="mono ${MONO_OPTIONS} ${HOME}/Projects/NUnit-2.6.1/bin/nunit-console.exe"
 
-${NUNIT} -noshadow -nothread dotnet/Pyrolite.Tests/bin/Debug/Pyrolite.Tests.exe
+${NUNIT} -framework:4.0 -noshadow -nothread dotnet/Pyrolite.Tests/bin/Debug/Pyrolite.Tests.exe
