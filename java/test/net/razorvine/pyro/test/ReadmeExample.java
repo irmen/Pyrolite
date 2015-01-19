5dffd5607e875edcfcec776cfd9068c3da94d9ee
Irmen de Jong <irmen@razorvine.net>
2015-01-19 22:29:54 +0100

    added example from readme.txt

---
+++ java/test/net/razorvine/pyro/test/ReadmeExample.java
@@ -0,0 +1,39 @@
+package net.razorvine.pyro.test;
+import java.io.IOException;
+
+import net.razorvine.pyro.*;
+
+public class ReadmeExample {
+
+	public static void main(String[] args) throws IOException
+	{
+	    NameServerProxy ns = NameServerProxy.locateNS(null);
+	    PyroProxy remoteobject = new PyroProxy(ns.lookup("Your.Pyro.Object"));
+	    Object result = remoteobject.call("pythonmethod", 42, "hello", new int[]{1,2,3});
+	    String message = (String)result;  // cast to the type that 'pythonmethod' returns
+	    System.out.println("result message="+message);
+	    remoteobject.close();
+	    ns.close();		
+	}
+}
+
+/**
+
+The above code works if you start a Pyro nameserver and run this Pyro server program:
+
+
+import Pyro4
+
+class Readme(object):
+    def pythonmethod(self, number, message, array):
+        print("got number", number)
+        print("got message", message)
+        print("got array", array)
+        return "all done!"
+
+Pyro4.Daemon.serveSimple({
+    Readme(): "Your.Pyro.Object"
+})
+
+
+**/
