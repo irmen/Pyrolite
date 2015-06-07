8d131ba1bc48e3a6c608d32827bf7c934e806046
Irmen de Jong <irmen@razorvine.net>
2015-06-07 16:53:15 +0200

    test release build

--- dotnet/test-dotnet.cmd
+++ dotnet/test-dotnet.cmd
@@ -1,4 +1,4 @@
 echo "Running tests"
 
-L:\tools\nunit2.6\nunit-console-x86 /framework:net-4.0 /nothread /noshadow .\Pyrolite.Tests\bin\Debug\Pyrolite.Tests.exe
+L:\tools\nunit2.6\nunit-console-x86 /framework:net-4.0 /nothread /noshadow .\Pyrolite.Tests\bin\Release\Pyrolite.Tests.exe
 
