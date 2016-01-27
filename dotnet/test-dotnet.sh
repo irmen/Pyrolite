5194bd566beaafa60e33db7187b5cbb86044eebc
Irmen de Jong <irmen@razorvine.net>
2016-01-27 01:37:08 +0100

    updated nunit console command

--- dotnet/test-dotnet.sh
+++ dotnet/test-dotnet.sh
@@ -3,11 +3,4 @@ echo "Building"
 . ./build-dotnet-mono.sh
 
 echo "Running tests"
-
-# we need to use the runtime 4.x version of nunit.
-# the default nunit-console command uses the framework 2.x version.
-
-NUNIT_CONSOLE=/Library/Frameworks/Mono.framework/Versions/Current/lib/mono/4.5/nunit-console.exe
-NUNIT="mono ${MONO_OPTIONS} ${NUNIT_CONSOLE}"
-
-${NUNIT} -noshadow Pyrolite.Tests/bin/Release/Pyrolite.Tests.exe
+nunit-console -noshadow Pyrolite.Tests/bin/Release/Pyrolite.Tests.exe
