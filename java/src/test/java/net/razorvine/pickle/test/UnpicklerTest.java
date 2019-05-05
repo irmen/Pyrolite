475448b79df8b63ad7f42d16e4893d7e902ea9e3
Irmen de Jong <irmen@razorvine.net>
2019-05-05 12:38:21 +0200

    tweaks

--- java/src/test/java/net/razorvine/pickle/test/UnpicklerTest.java
+++ java/src/test/java/net/razorvine/pickle/test/UnpicklerTest.java
@@ -31,7 +31,7 @@ import org.junit.Ignore;
 
 /**
  * Unit tests for the unpickler.
- *  
+ *
  * @author Irmen de Jong (irmen@razorvine.net)
  */
 public class UnpicklerTest {
@@ -46,17 +46,17 @@ public class UnpicklerTest {
 
 	Object U(String strdata) throws PickleException, IOException
 	{
-		return U(PickleUtils.str2bytes(strdata));	
+		return U(PickleUtils.str2bytes(strdata));
 	}
 	Object U(byte[] data) throws PickleException, IOException
 	{
 		Unpickler u=new Unpickler();
 		Object o=u.loads(data);
 		u.close();
-		return o;		
+		return o;
 	}
-	
-	
+
+
 	@Test
 	public void testSinglePrimitives() throws PickleException, IOException {
 		// protocol level 1
@@ -100,20 +100,20 @@ public class UnpicklerTest {
 		assertArrayEquals(new byte[]{'a','b','c'}, (byte[]) U("B\u0003\u0000\u0000\u0000abc."));
 		assertArrayEquals(new byte[]{'a','b','c'}, (byte[]) U("C\u0003abc."));
 	}
-	
+
 	@Test
 	public void testUnicodeStrings() throws PickleException, IOException
 	{
 		assertEquals("\u00ff", U("S'\\xff'\n."));
 		assertEquals("\u20ac", U("V\\u20ac\n."));
-		
+
 		assertEquals("euro\u20ac", U("X\u0007\u0000\u0000\u0000euro\u00e2\u0082\u00ac."));   // utf-8 encoded
-		
+
 		assertEquals("\u0007\u00db\u007f\u0080",U(new byte[]{'T',0x04,0x00,0x00,0x00,0x07,(byte)0xdb,0x7f,(byte)0x80,'.'}));  // string with non-ascii symbols
 		assertEquals("\u0007\u00db\u007f\u0080",U(new byte[]{'U',0x04,0x07,(byte)0xdb,0x7f,(byte)0x80,'.'}));  // string with non-ascii symbols
 		assertEquals("\u0007\u00db\u007f\u0080",U(new byte[]{'V',0x07,(byte)0xdb,0x7f,(byte)0x80,'\n','.'}));  // string with non-ascii symbols
 	}
-	
+
 	@Test
 	public void testTuples() throws PickleException, IOException
 	{
@@ -128,7 +128,7 @@ public class UnpicklerTest {
 	public void testLists() throws PickleException, IOException
 	{
 		ArrayList<Integer> list=new ArrayList<Integer>(0);
-		
+
 		assertEquals(list, U("]."));	// []
 		list.add(97);
 		assertEquals(list, U("]Kaa."));	// [97]
@@ -137,7 +137,7 @@ public class UnpicklerTest {
 		list.add(99);
 		assertEquals(list, U("](KaKbKce."));	// [97,98,99]
 	}
-	
+
 	@Test
 	public void testDicts() throws PickleException, IOException
 	{
@@ -150,7 +150,7 @@ public class UnpicklerTest {
 		assertEquals(map, U("}(KaKbKcKdu."));  // {97: 98, 99: 100}
 		assertEquals(map, U("(dI97\nI98\nsI99\nI100\ns.")); // {97: 98, 99: 100}
 		assertEquals(map, U("(I97\nI98\nI99\nI100\nd.")); // {97: 98, 99: 100}
-	
+
 		map.clear();
 		map.put(1,2);
 		map.put(3,4);
@@ -160,11 +160,11 @@ public class UnpicklerTest {
 		list.add(map2);
 		assertEquals(list, U("(lp0\n(dp1\nI1\nI2\nsI3\nI4\nsa(dp2\nI5\nI6\nsI7\nI8\nsa."));  // [{1:2, 3:4}, {5:6, 7:8}]
 		assertEquals(list, U("\u0080\u0002]q\u0000(}q\u0001(K\u0001K\u0002K\u0003K\u0004u}q\u0002(K\u0005K\u0006K\u0007K\u0008ue."));  // [{1:2, 3:4}, {5:6, 7:8}]
-		
+
 		map.clear();
 		map2.clear();
 		list.clear();
-		
+
 		map.put("abc",null);
 		assertEquals(map, U("(dp0\nS'abc'\np1\nNs.")); // {'abc': None}
 		assertEquals(map, U("\u0080\u0002}q\u0000U\u0003abcq\u0001Ns.")); // {'abc': None}
@@ -187,7 +187,7 @@ public class UnpicklerTest {
 		assertEquals(map, U("(dp0\nS'abc'\np1\n(dp2\nS'def'\np3\n(lp4\nI111\naI111\nass.")); // {'abc': {'def': [111,111] }}
 		assertEquals(map, U("\u0080\u0002}q\u0000U\u0003abcq\u0001}q\u0002U\u0003defq\u0003]q\u0004(KoKoess.")); // {'abc': {'def': [111,111] }}
 
-		
+
 		ArrayList<Object> list2=new ArrayList<Object>();
 		list2.add(222);
 		list2.add(222);
@@ -201,7 +201,7 @@ public class UnpicklerTest {
 		assertEquals(map, U("(dp0\nS'abc'\np1\n(dp2\ng1\n(lp3\nI111\naI111\nasS'def'\np4\ng3\nss.")); // {'abc': {'def': [111,111], abc: [111,111] }}
 		assertEquals(map, U("\u0080\u0002}q\u0000U\u0003abcq\u0001}q\u0002(h\u0001]q\u0003(KoKoeU\u0003defq\u0004h\u0003us.")); // {'abc': {'def': [111,111], abc: [111,111] }}
 	}
-	
+
 	@Test
 	public void testComplex() throws PickleException, IOException
 	{
@@ -209,7 +209,7 @@ public class UnpicklerTest {
 		assertEquals(c, U("c__builtin__\ncomplex\np0\n(F2.0\nF4.0\ntp1\nRp2\n."));
 		assertEquals(c, U("c__builtin__\ncomplex\nq\u0000G@\u0000\u0000\u0000\u0000\u0000\u0000\u0000G@\u0010\u0000\u0000\u0000\u0000\u0000\u0000\u0086q\u0001Rq\u0002."));
 	}
-	
+
 	@Test
 	public void testDecimal() throws PickleException, IOException
 	{
@@ -230,7 +230,7 @@ public class UnpicklerTest {
 		assertEquals(59, time.seconds);
 		assertEquals(456000, time.microseconds);
 		assertEquals("Time: 14 hours, 33 minutes, 59 seconds, 456000 microseconds", time.toString());
-		
+
 		c=new GregorianCalendar(2011, Calendar.DECEMBER, 31);
 		c.set(Calendar.HOUR_OF_DAY, 14);
 		c.set(Calendar.MINUTE, 33);
@@ -240,7 +240,7 @@ public class UnpicklerTest {
 		assertEquals(c.getTimeInMillis(), pc.getTimeInMillis());
 		pc=(Calendar) U("cdatetime\ndatetime\np0\n(S'\\x07\\xdb\\x0c\\x1f\\x0e!;\\x06\\xf5@'\np1\ntp2\nRp3\n.");
 		assertEquals(c.getTimeInMillis(), pc.getTimeInMillis());
-		
+
 		c=new GregorianCalendar(2014, Calendar.JULY, 8);
 		c.set(Calendar.HOUR_OF_DAY, 10);
 		c.set(Calendar.MINUTE, 10);
@@ -255,7 +255,7 @@ public class UnpicklerTest {
 		assertEquals(456789, td.microseconds);
 		assertEquals("Timedelta: 729 days, 53973 seconds, 456789 microseconds (total: 63039573.456789 seconds)", td.toString());
 	}
-	
+
 	@Test
 	public void testDateTimePython3() throws PickleException, IOException
 	{
@@ -274,7 +274,7 @@ public class UnpicklerTest {
 		assertEquals(59,time.seconds);
 		assertEquals(456000,time.microseconds);
 		assertEquals("Time: 14 hours, 33 minutes, 59 seconds, 456000 microseconds", time.toString());
-		
+
 		c=new GregorianCalendar(2011, Calendar.DECEMBER, 31);
 		c.set(Calendar.HOUR_OF_DAY, 14);
 		c.set(Calendar.MINUTE, 33);
@@ -297,7 +297,7 @@ public class UnpicklerTest {
 		Unpickler u = new Unpickler();
 		Calendar c2 = (Calendar) u.loads(pickle);
 		assertEquals(c, c2);
-		
+
 		c=new GregorianCalendar(2011, Calendar.OCTOBER, 9);
 		c.set(Calendar.HOUR_OF_DAY, 13);
 		c.set(Calendar.MINUTE, 10);
@@ -310,7 +310,7 @@ public class UnpicklerTest {
 		c2 = (Calendar) U("cdatetime\ndatetime\np0\n(S\"\\x07\\xdb\\n\\t\\r\\n\\t\\x00\'\\x10\"\np1\ntp2\nRp3\n.");	// protocol 0
 		assertEquals(c, c2);
 	}
-	
+
 	@Test
 	public void testDateTimeWithTimezones() throws PickleException, IOException
 	{
@@ -361,7 +361,7 @@ public class UnpicklerTest {
 		assertEquals(c.getTimeInMillis(), pc.getTimeInMillis());
 		pc=(Calendar) U("\u0080\u0002cdatetime\ndatetime\nq\u0000U\n\u0007\u00de\u0007\u0008\n\n\u0000\u0000\u0000\u0000q\u0001cpytz\n_p\nq\u0002U\u0003MSTq\u0003\u0085q\u0004Rq\u0005\u0086q\u0006Rq\u0007.");
 		assertEquals(c.getTimeInMillis(), pc.getTimeInMillis());
-		
+
 		// dateutil gettz timezone
 		tz = TimeZone.getTimeZone("Europe/Amsterdam");
 		c=new GregorianCalendar(2015, Calendar.APRIL, 9);
@@ -384,7 +384,7 @@ public class UnpicklerTest {
 		pc=(Calendar) U("\u0080\u0002cdatetime\ndatetime\nU\n\u0007\u00df\u0004\u0006\u000e*.\u0000a\u00a8cdateutil.zoneinfo\ngettz\nU\u0010Europe/Amsterdam\u0085R\u0086R.");
 		assertEquals("Europe/Amsterdam", pc.getTimeZone().getID());
 	}
-	
+
 	@Test
 	public void testCodecBytes() throws IOException
 	{
@@ -392,7 +392,7 @@ public class UnpicklerTest {
 		byte[] data = (byte[]) U("\u0080\u0002c_codecs\nencode\nX\u0004\u0000\u0000\u0000testX\u0006\u0000\u0000\u0000latin1\u0086R.");
 		assertArrayEquals("test".getBytes(), data);
 	}
-	
+
 	@Test
 	public void testBytesAndByteArray() throws IOException
 	{
@@ -401,7 +401,7 @@ public class UnpicklerTest {
 		assertArrayEquals(bytes, (byte[])U("c__builtin__\nbytearray\np0\n(V\u0001\u0002\u007f\u0080\u00ff\np1\nS'latin-1'\np2\ntp3\nRp4\n."));
 		bytes=new byte[]{1,2,3};
 		assertArrayEquals(bytes, (byte[])U("\u0080\u0002c__builtin__\nbytearray\nX\u0003\u0000\u0000\u0000\u0001\u0002\u0003X\n\u0000\u0000\u0000iso-8859-1\u0086R."));
-		
+
 		// the following bytecode pickle has been created in python by pickling a bytearray
 		// from 0x00 to 0xff with protocol level 0.
 		byte[] p0=new byte[] {
@@ -424,7 +424,7 @@ public class UnpicklerTest {
 			(byte)0xf6,(byte)0xf7,(byte)0xf8,(byte)0xf9,(byte)0xfa,(byte)0xfb,(byte)0xfc,(byte)0xfd,(byte)0xfe,(byte)0xff,
 			0xa,0x70,0x31,0xa,0x53,0x27,0x6c,0x61,0x74,0x69,0x6e,0x2d,0x31,0x27,0xa,0x70,0x32,0xa,0x74,0x70,0x33,0xa,0x52,0x70,0x34,0xa,0x2e
 		};
-		
+
 		bytes=new byte[256];
 		for(int i=0; i<256; ++i)
 			bytes[i]=(byte)i;
@@ -466,7 +466,7 @@ public class UnpicklerTest {
 			0x55,0x7,0x6c,0x61,0x74,0x69,0x6e,0x2d,0x31,0x71,0x2,(byte)0x86,0x71,0x3,0x52,0x71,0x4,0x2e
 		};
 		assertArrayEquals(bytes, (byte[])U(p2));
-		
+
 		// the following is a python3 pickle of a small bytearray. It constructs a bytearray using [SHORT_]BINBYTES instead of a list.
 		byte[] p3 = new byte[] {
 				(byte)0x80, 0x04, (byte)0x95, 0x23, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, (byte)0x8c, 0x08, 0x62, 0x75, 0x69,
@@ -475,7 +475,7 @@ public class UnpicklerTest {
 		};
 		assertArrayEquals(new byte[]{'A','B','C','D','E','F','G','H'}, (byte[])U(p3));
 	}
-	
+
 	@Test
 	public void testArray() throws PickleException, IOException
 	{
@@ -486,7 +486,7 @@ public class UnpicklerTest {
 		testc=new char[]{'x','y','z'};
 		arrayc=(char[])U("carray\narray\nU\u0001c](U\u0001xU\u0001yU\u0001ze\u0086R.");
 		assertArrayEquals(testc,arrayc);
-		
+
 		// u=unicode char
 		testc=new char[]{'a','b','c'};
 		arrayc=(char[]) U("carray\narray\np0\n(S'u'\np1\n(lp2\nVa\np3\naVb\np4\naVc\np5\natp6\nRp7\n.");
@@ -505,7 +505,7 @@ public class UnpicklerTest {
 		tests=new short[]{1,2,128,255,32700,-32700};
 		arrays=(short[]) U("carray\narray\np0\n(S'h'\np1\n(lp2\nI1\naI2\naI128\naI255\naI32700\naI-32700\natp3\nRp4\n.");
 		assertArrayEquals(tests,arrays);
-		
+
 		// H=unsigned integer 2
 		int[] testi=new int[]{1,2,40000,65535};
 		int[] arrayi=(int[]) U("carray\narray\np0\n(S'H'\np1\n(lp2\nI1\naI2\naI40000\naI65535\natp3\nRp4\n.");
@@ -515,7 +515,7 @@ public class UnpicklerTest {
 		testi=new int[]{1,2,999999999,-999999999};
 		arrayi=(int[]) U("carray\narray\np0\n(S'i'\np1\n(lp2\nI1\naI2\naI999999999\naI-999999999\natp3\nRp4\n.");
 		assertArrayEquals(testi,arrayi);
-		
+
 		// l=signed integer 4
 		testi=new int[]{1,2,999999999,-999999999};
 		arrayi=(int[]) U("carray\narray\np0\n(S'l'\np1\n(lp2\nI1\naI2\naI999999999\naI-999999999\natp3\nRp4\n.");
@@ -541,22 +541,22 @@ public class UnpicklerTest {
 		double[] arrayd=(double[]) U("carray\narray\np0\n(S'd'\np1\n(lp2\nF-4.4\naF4.4\natp3\nRp4\n.");
 		assertArrayEquals(testd,arrayd,0.000001);
 	}
-	
+
 	@Test
 	public void testArrayPython3() throws IOException, PickleException {
 		// python 3 array reconstructor
 		short[] testi=new short[]{1,2,3};
 		short[] arrayi=(short[])U("\u0080\u0003carray\n_array_reconstructor\nq\u0000(carray\narray\nq\u0001X\u0001\u0000\u0000\u0000hq\u0002K\u0004C\u0006\u0001\u0000\u0002\u0000\u0003\u0000q\u0003tq\u0004Rq\u0005.");
-							  
+
 		assertArrayEquals(testi, arrayi);
 	}
-	
+
 	@Test(expected=PickleException.class)
 	public void testArrayPython26NotSupported() throws PickleException, IOException {
 		U("carray\narray\n(S'h'\nS'\\x01\\x00\\x02\\x00\\x03\\x00'\ntR.");
 		fail("should crash with pickle exception because not supported");
 	}
-	
+
 	@Test
 	@SuppressWarnings("unchecked")
 	public void testSet() throws PickleException, IOException
@@ -565,30 +565,30 @@ public class UnpicklerTest {
 		set.add(1);
 		set.add(2);
 		set.add("abc");
-		
+
 		assertEquals(set,(Set<Object>)U("c__builtin__\nset\np0\n((lp1\nI1\naI2\naS'abc'\np2\natp3\nRp4\n."));
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	@Test
 	public void testMemoing() throws PickleException, IOException
 	{
 	    ArrayList<Object> list=new ArrayList<Object>();
-        list.add("irmen");	 
-        list.add("irmen");	 
-        list.add("irmen");	 
+        list.add("irmen");
+        list.add("irmen");
+        list.add("irmen");
 		assertEquals(list, U("]q\u0000(U\u0005irmenq\u0001h\u0001h\u0001e."));
-		
+
 		ArrayList<Object>a=new ArrayList<Object>();
 		a.add(111);
 		ArrayList<Object>b=new ArrayList<Object>();
 		b.add(222);
 		ArrayList<Object>c=new ArrayList<Object>();
 		c.add(333);
-		
+
 		Object[] array=new Object[] {a,b,c,a,b,c};
 		assertArrayEquals(array, (Object[]) U("((lp0\nI111\na(lp1\nI222\na(lp2\nI333\nag0\ng1\ng2\ntp3\n."));
-		
+
 		list.clear();
 		list.add("a");
 		list.add("b");
@@ -600,7 +600,7 @@ public class UnpicklerTest {
 		a=(ArrayList<Object>)U("]q\u0000(]q\u0001(K\u0001K\u0002K\u0003e]q\u0002(h\u0001h\u0001ee.");
 		assertEquals("[[1, 2, 3], [[1, 2, 3], [1, 2, 3]]]", a.toString());
 	}
-	
+
 	@Test
 	public void testBinint2WithObject() throws PickleException, IOException
 	{
@@ -609,7 +609,7 @@ public class UnpicklerTest {
 		int result=(Integer) u.loads(data);
 		assertEquals(9090,result);
 	}
-	
+
     @Test
     @Ignore("performancetest")
     public void testUnpicklingPerformance() throws PickleException, IOException {
@@ -633,7 +633,7 @@ public class UnpicklerTest {
     }
 
 	@Test
-	public void testNestedLists() throws PickleException, IOException
+	public void testUnpicklerNoMapReuse() throws PickleException, IOException
 	{
 		byte[][] array = new byte[2][20];
 
@@ -646,25 +646,26 @@ public class UnpicklerTest {
 		// We reuse this Unpickler to unpickle each bytes without calling close.
 		Unpickler unpickler = new Unpickler();
 
-		for (int bytesIdx = 0; bytesIdx < array.length; bytesIdx++) {
-			ArrayList<ArrayList<Object>> pickled1 = (ArrayList<ArrayList<Object>>) unpickler.loads(array[bytesIdx]);
-			ArrayList<ArrayList<Object>> pickled2 = (ArrayList<ArrayList<Object>>) U(array[bytesIdx]);
+        for (byte[] bytes : array) {
+            ArrayList<ArrayList<Object>> pickled1 = (ArrayList<ArrayList<Object>>) unpickler.loads(bytes);
+            Unpickler u2 = new Unpickler();
+            ArrayList<ArrayList<Object>> pickled2 = (ArrayList<ArrayList<Object>>) u2.loads(bytes);
+            u2.close();
 
-			Iterator iter1 = pickled1.iterator();
-			Iterator iter2 = pickled2.iterator();
+            Iterator iter1 = pickled1.iterator();
+            Iterator iter2 = pickled2.iterator();
 
-			while (iter1.hasNext()) {
-				Object[] obj1 = (Object[]) iter1.next();
-				Object[] obj2 = (Object[]) iter2.next();
+            while (iter1.hasNext()) {
+                Object[] obj1 = (Object[]) iter1.next();
+                Object[] obj2 = (Object[]) iter2.next();
 
-				for (int idx = 0; idx < obj1.length; idx++) {
-					assertEquals(expected, (ArrayList<Integer>)obj1[idx]);
-				}
+                for (Object o : obj1) {
+                    assertEquals(expected, o);
+                }
 
-				// Unpickled results from an reused Unpickler should be the same as the Unpickler
-				// that closes after each unpickling.
-				assertArrayEquals(obj1, obj2);
-			}
-		}
+                // results from a reused Unpickler should be the same as the Unpickler that closes after each unpickling.
+                assertArrayEquals(obj1, obj2);
+            }
+        }
 	}
 }
