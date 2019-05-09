4977e5c70088987fdd137b136eaf1ccdb2bfb861
Adam Sitnik <adam.sitnik@gmail.com>
2019-05-09 12:38:16 +0200

    redesign the benchmarks

--- dotnet/Razorvine.Pyrolite/Benchmarks/PicklerBenchmarks.cs
+++ dotnet/Razorvine.Pyrolite/Benchmarks/PicklerBenchmarks.cs
@@ -1,45 +1,69 @@
 ﻿using BenchmarkDotNet.Attributes;
 using Razorvine.Pickle;
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
 namespace Benchmarks
 {
-    public class PicklerBenchmarks
+    [GenericTypeArguments(typeof(string))]
+    [GenericTypeArguments(typeof(double))]
+    [GenericTypeArguments(typeof(int))]
+    [GenericTypeArguments(typeof(bool))]
+    public class PicklerBenchmarks<T>
     {
-        [Params(100, 1000)]
+        [Params(1000)]
         public int Count { get; set; }
 
-        private double[] _doubles;
-        private int[] _integers;
-        private bool[] _booleans;
-        private string[] _strings;
+        [Params(false)]
+        public bool UseMemo { get; set; }
 
+        [Params(false, true)]
+        public bool Boxed { get; set; }
+
+        private object _value;
+        private Pickler _pickler;
         private byte[] _reusable;
 
         [GlobalSetup]
         public void Setup()
         {
-            _doubles = Enumerable.Range(0, Count).Select(x => x * 0.5).ToArray();
-            _integers = Enumerable.Range(0, Count).ToArray();
-            _booleans = Enumerable.Range(0, Count).Select(x => x % 2 == 0).ToArray();
-            _strings = _doubles.Select(x => x.ToString()).ToArray();
+            IEnumerable<T> input = Enumerable.Range(0, Count).Select(GetValue);
+
+            if (Boxed) // a common case in dotnet/spark
+                _value = input.Select(x => (object)x).ToArray();
+            else
+                _value = input.ToArray();
+
             _reusable = new byte[Count * 10];
+            _pickler = new Pickler(UseMemo);
         }
 
-        [Benchmark] public byte[] DoublesToByteArray() => new Pickler().dumps(_doubles);
-        [Benchmark] public byte[] IntegersToByteArray() => new Pickler().dumps(_integers);
-        [Benchmark] public byte[] BooleansToByteArray() => new Pickler().dumps(_booleans);
-        [Benchmark] public byte[] StringsToByteArray() => new Pickler().dumps(_strings);
+        [GlobalCleanup]
+        public void Cleanup() => _pickler.Dispose();
 
-        [Benchmark] public void DoublesToReusableByteArray() => new Pickler().dumps(_doubles, ref _reusable, out _);
-        [Benchmark] public void IntegersToReusableByteArray() => new Pickler().dumps(_integers, ref _reusable, out _);
-        [Benchmark] public void BooleansToReusableByteArray() => new Pickler().dumps(_booleans, ref _reusable, out _);
-        [Benchmark] public void StringsToReusableByteArray() => new Pickler().dumps(_strings, ref _reusable, out _);
+        [Benchmark]
+        public byte[] ToByteArray() => _pickler.dumps(_value);
 
-        [Benchmark] public void DoublesToStream() => new Pickler().dump(_doubles, new MemoryStream());
-        [Benchmark] public void IntegersToStream() => new Pickler().dump(_integers, new MemoryStream());
-        [Benchmark] public void BooleansToStream() => new Pickler().dump(_booleans, new MemoryStream());
-        [Benchmark] public void StringsToStream() => new Pickler().dump(_strings, new MemoryStream());
+        [Benchmark]
+        public void ToReusableByteArray() => _pickler.dumps(_value, ref _reusable, out _);
+
+        [Benchmark]
+        public void ToStream() => _pickler.dump(_value, new MemoryStream());
+
+        private static T GetValue(int index)
+        {
+            if (typeof(T) == typeof(int))
+                return (T)(object)index;
+            if (typeof(T) == typeof(double))
+                return (T)(object)(index * 0.5);
+            if (typeof(T) == typeof(bool))
+                return (T)(object)(index % 2 == 0);
+            if (typeof(T) == typeof(string))
+                return (T)(object)index.ToString();
+
+            throw new NotSupportedException();
+        }
     }
 }
